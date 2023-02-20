5dabf1428b1d7963a9324fd6b94bc98ff26506b7
Alexander Kazakov <alexandkazakov@ya.ru>
2023-02-20 17:45:12 +0300

    sync input[text] and input[range] fixed

--- src/script/index.js
+++ src/script/index.js
@@ -7,15 +7,37 @@ const inputs = document.querySelectorAll('.form__input');
 const ranges = document.querySelectorAll('.form__range');
 
 for (let i = 0; i < inputs.length; ++i) {
+
+  function formatValue(value) {
+    let numberValue = value.replace(/\s*/g, '');
+    let formatedNumberValue = new Intl.NumberFormat('ru-RU').format(value);
+
+    return {
+      numberValue,
+      formatedNumberValue
+    }
+  }
+
   function setRange(value) {
-    const formatValue = new Intl.NumberFormat('ru-RU').format(value)
-    inputs[i].value = formatValue;
+    value = formatValue(value).numberValue;
+    let formatedValue = formatValue(value).formatedNumberValue;
+
+    inputs[i].value = formatedValue;
     ranges[i].value = value;
     inputs[i].textContent = value;
   }
 
   ranges[i].addEventListener('input', () => setRange(ranges[i].value));
-  inputs[i].addEventListener('change', () => setRange(inputs[i].value));
+  inputs[i].addEventListener('input', () => {
+    setRange(inputs[i].value);
+    ranges[i].style.setProperty('--value', ranges[i].value);
+
+    if (+formatValue(inputs[i].value).numberValue < ranges[i].getAttribute('min')) {
+      inputs[i].value = formatValue(ranges[i].getAttribute('min')).formatedNumberValue;
+    } else if (+formatValue(inputs[i].value).numberValue > ranges[i].getAttribute('max')) {
+      inputs[i].value = formatValue(ranges[i].getAttribute('max')).formatedNumberValue;
+    }
+  });
 }
 
 ranges.forEach( range => {
@@ -23,4 +45,4 @@ ranges.forEach( range => {
   range.style.setProperty('--min', range.min == '' ? '0' : range.min);
   range.style.setProperty('--max', range.max == '' ? '100' : range.max);
   range.addEventListener('input', () => range.style.setProperty('--value', range.value));
-})
+});
